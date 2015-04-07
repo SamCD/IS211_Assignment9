834a1f731cfa67b14675395a7aeaa016601e4fef
SamCD <samuel.cohen-devries@spsmail.cuny.edu>
2015-04-07 16:44:46 -0400

    Create apple_stock.py

---
+++ apple_stock.py
@@ -0,0 +1,11 @@
+page = urlopen('http://finance.yahoo.com/q/hp?s=AAPL+Historical+Prices')
+soup = BeautifulSoup(page)
+links = soup.find_all('td',attrs={'class':'yfnc_tabledata1'})
+closes = {}
+for link in links:
+    try:
+        if link.has_attr('nowrap') and not link.has_attr('colspan'):
+            closes[link.text] = link.find_next_siblings(
+                )[3].text
+    except IndexError:
+        continue
